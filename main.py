cfa97e453541a84b5f536ab56961d050ea326e7a
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-01-15 20:49:51 +0700

    change host

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ def get_folder_length(bucket_name, folder_prefix):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
